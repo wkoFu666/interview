7ac99417eb02121ee7b7b5e507236104b187d129
fuxt <fuxt@si-tech.com.cn>
2023-01-09 16:42:46 +0800

    提交

--- src/main/java/com/wko/dothings/wallet/constant/CommonConstant.java
+++ src/main/java/com/wko/dothings/wallet/constant/CommonConstant.java
@@ -2,8 +2,10 @@ package com.wko.dothings.wallet.constant;
 
 /**
  * 存放常量
+ * @author 90310
  */
 public interface CommonConstant {
 
+    public static final String DESCRIPTION="this is a description.";
 
 }
